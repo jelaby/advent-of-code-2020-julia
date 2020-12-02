26117ad7ea701da619ead381e64f4657c3079221
Paul Mealor <paul.mealor@gmail.com>
2020-12-02 13:21:03 +0000

    Day 2 part 1

--- src/day2.jl
+++ src/day2.jl
@@ -4,3 +4,30 @@ day2:
 - Author: Paul.Mealor
 - Date: 2020-12-02
 =#
+
+using StatsBase
+
+struct PasswordSpec
+    min::Int
+    max::Int
+    char::AbstractChar
+    password::AbstractString
+end
+
+function PasswordSpec(min::AbstractString, max::AbstractString, char::AbstractString, password::AbstractString)
+    PasswordSpec(parse(Int, min), parse(Int, max), char[1], password)
+end
+
+function PasswordSpec(line)
+    PasswordSpec(match(r"^(\d+)-(\d+)\s+(\w):\s*(\w+)$", line).captures...)
+end
+
+passwords = open("src/day2-input.txt") do file
+    readlines(file) |> l -> PasswordSpec.(l)
+end
+
+function checkPassword(spec :: PasswordSpec)
+    return spec.min <= get(countmap(spec.password), spec.char, 0) <= spec.max
+end
+
+(+)(checkPassword.(passwords)...)
