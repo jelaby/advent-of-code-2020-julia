27886d4996bfbb54f411147782669bf1ce25ef92
Paul Mealor <paul.mealor@gmail.com>
2020-12-04 16:40:13 +0000

    Day 4: sh golf

--- src/day4.sh
+++ src/day4.sh
@@ -2,13 +2,13 @@
 
 
 byr='\<byr:(19[2-9][0-9]|200[0-2])\>' # (Birth Year) - four digits; at least 1920 and at most 2002.
-iyr='\<iyr:(201[0-9]|2020)\>' # (Issue Year) - four digits; at least 2010 and at most 2020.
-eyr='\<eyr:(202[0-9]|2030)\>' # (Expiration Year) - four digits; at least 2020 and at most 2030.
-hgt='\<hgt:(1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in)\>' # (Height) - a number followed by either cm or in:
+iyr='\<iyr:20(1[0-9]|20)\>' # (Issue Year) - four digits; at least 2010 and at most 2020.
+eyr='\<eyr:20(2[0-9]|30)\>' # (Expiration Year) - four digits; at least 2020 and at most 2030.
+hgt='\<hgt:(1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in)\>' # (Height) - a number followed by either cm or in:
 # If cm, the number must be at least 150 and at most 193.
 # If in, the number must be at least 59 and at most 76.
 hcl='\<hcl:#[0-9a-f]{6}\>' # (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-ecl='\<ecl:(amb|blu|brn|gry|grn|hzl|oth)\>' # (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+ecl='\<ecl:(amb|b(lu|rn)|gr[yn]|hzl|oth)\>' # (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
 pid='\<pid:[0-9]{9}\>' # (Passport ID) - a nine-digit number, including leading zeroes.
 # cid (Country ID) - ignored, missing or not.
 
@@ -16,4 +16,4 @@ cmd="sed -nE '/./H;\$b c;/^$/b c;b;:c;{x;s/\\n/ /gm;/$byr/{/$iyr/{/$eyr/{/$hgt/{
 
 bash -c "$cmd"
 bash -c "$cmd | wc -l"
-echo "$cmd"
+echo "$cmd | wc -l"
