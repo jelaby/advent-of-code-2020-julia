ec7f55eea028643f61b6dee0502ad526a024eaca
Paul Mealor <paul.mealor@gmail.com>
2020-12-12 10:18:47 +0000

    Day 12

---
+++ src/day12.jl
@@ -0,0 +1,57 @@
+#=
+day12:
+- Julia version: 1.5.2
+- Author: Paul.Mealor
+- Date: 2020-12-12
+=#
+
+using Test
+import AoC
+
+north=[1 0]
+east=[0 1]
+south=[-1 0]
+west=[0 -1]
+
+left=[0 -1;1 0]
+right=[0 1;-1 0]
+
+struct Ship
+    heading::Array
+    position::Array
+    Ship(heading::Array, position::Array) = new(sign.(heading), position)
+end
+Ship() = Ship(east,[0 0])
+
+function rotate(ship, direction, degrees)
+    heading = ship.heading
+    for _ in 90:90:degrees
+        heading = heading * direction
+    end
+    return Ship(heading, ship.position)
+end
+
+navigate(ship, direction, distance) = Ship(ship.heading, ship.position + (distance*direction))
+navigate(ship, ::Val{:N}, arg) = navigate(ship, north, arg)
+navigate(ship, ::Val{:E}, arg) = navigate(ship, east, arg)
+navigate(ship, ::Val{:S}, arg) = navigate(ship, south, arg)
+navigate(ship, ::Val{:W}, arg) = navigate(ship, west, arg)
+navigate(ship, ::Val{:L}, arg) = rotate(ship, left, arg)
+navigate(ship, ::Val{:R}, arg) = rotate(ship, right, arg)
+navigate(ship, ::Val{:F}, arg) = navigate(ship, ship.heading, arg)
+
+instruction(t) = (Val(Symbol(t[1:1])), parse(Int, t[2:end]))
+
+function navigate(lines)
+    ship = Ship()
+    for line in lines
+        ship = navigate(ship, instruction(line)...)
+    end
+    return ship
+end
+
+manhattan(a) = sum(abs.(a))
+
+@test navigate(AoC.exampleLines(12,1)).position == [-8 17]
+@test manhattan(navigate(AoC.exampleLines(12,1)).position) == 25
+@show manhattan(navigate(AoC.lines(12)).position)
